35b16aef049d435bf30377154cf027c38ea96294
Daniel <mail@danielluedecke.de>
2020-04-01 19:05:00 +0200

    update

--- server.R
+++ server.R
@@ -3,10 +3,10 @@ library(ggplot2)
 library(ggrepel)
 library(shiny)
 
-get_country_data <- function(country, include_death = FALSE) {
+get_country_data <- function(country, include_death = FALSE, separate_death = FALSE) {
   x <- coronavirus
 
-  if (include_death) {
+  if (isTRUE(include_death) && isFALSE(separate_death)) {
     x$type[x$type == "death"] <- "recovered"
     x <- x %>%
       filter(country == {{country}}) %>%
@@ -18,6 +18,17 @@ get_country_data <- function(country, include_death = FALSE) {
     x$count <- NA
     x$count[tail(which(x$type == "confirmed"), 1)] <- x$cases[tail(which(x$type == "confirmed"), 1)]
     x$count[tail(which(x$type == "recovered"), 1)] <- x$cases[tail(which(x$type == "recovered"), 1)]
+  } else if (isTRUE(include_death) && isTRUE(separate_death)) {
+    x <- x %>%
+      ungroup() %>%
+      filter(country == {{country}}) %>%
+      mutate(region = {{country}}) %>%
+      select(-country)
+
+    x$count <- NA
+    x$count[nrow(x) - 2] <- x$cases[nrow(x) - 2]
+    x$count[nrow(x) - 1] <- x$cases[nrow(x) - 1]
+    x$count[nrow(x)] <- x$cases[nrow(x)]
   } else {
     x <- x %>%
       ungroup() %>%
@@ -77,71 +88,83 @@ shinyServer(function(input, output) {
     }
 
     if (input$cBrazil) {
-      dat <- rbind(dat, get_country_data("Brazil", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Brazil", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cChina) {
-      dat <- rbind(dat, get_country_data("China", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("China", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cGermany) {
-      dat <- rbind(dat, get_country_data("Germany", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Germany", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cItaly) {
-      dat <- rbind(dat, get_country_data("Italy", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Italy", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cFrance) {
-      dat <- rbind(dat, get_country_data("France", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("France", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
+    }
+
+    if (input$cFinland) {
+      dat <- rbind(dat, get_country_data("Finland", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
+    }
+
+    if (input$cSweden) {
+      dat <- rbind(dat, get_country_data("Sweden", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
+    }
+
+    if (input$cRussia) {
+      dat <- rbind(dat, get_country_data("Russia", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cUS) {
-      dat <- rbind(dat, get_country_data("US", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("US", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cSpain) {
-      dat <- rbind(dat, get_country_data("Spain", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Spain", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cAustria) {
-      dat <- rbind(dat, get_country_data("Austria", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Austria", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cAustralia) {
-      dat <- rbind(dat, get_country_data("Australia", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Australia", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cSingapore) {
-      dat <- rbind(dat, get_country_data("Singapore", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Singapore", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cDenmark) {
-      dat <- rbind(dat, get_country_data("Denmark", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Denmark", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cIsrael) {
-      dat <- rbind(dat, get_country_data("Israel", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Israel", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cIran) {
-      dat <- rbind(dat, get_country_data("Iran", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Iran", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cUK) {
-      dat <- rbind(dat, get_country_data("United Kingdom", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("United Kingdom", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cPoland) {
-      dat <- rbind(dat, get_country_data("Poland", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Poland", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cNetherlands) {
-      dat <- rbind(dat, get_country_data("Netherlands", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Netherlands", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     if (input$cSouthKorea) {
-      dat <- rbind(dat, get_country_data("Korea, South", include_death = input$cIncludeDeath))
+      dat <- rbind(dat, get_country_data("Korea, South", include_death = input$cIncludeDeath, separate_death = input$cSeparateDeath))
     }
 
     dat
@@ -155,6 +178,7 @@ shinyServer(function(input, output) {
     if (input$cBrazil) x <- x + 1
     if (input$cChina) x <- x + 1
     if (input$cDenmark) x <- x + 1
+    if (input$cFinland) x <- x + 1
     if (input$cFrance) x <- x + 1
     if (input$cGermany) x <- x + 1
     if (input$cIran) x <- x + 1
@@ -162,9 +186,11 @@ shinyServer(function(input, output) {
     if (input$cItaly) x <- x + 1
     if (input$cNetherlands) x <- x + 1
     if (input$cPoland) x <- x + 1
+    if (input$cRussia) x <- x + 1
     if (input$cSingapore) x <- x + 1
     if (input$cSpain) x <- x + 1
     if (input$cSouthKorea) x <- x + 1
+    if (input$cSweden) x <- x + 1
     if (input$cUK) x <- x + 1
     if (input$cUS) x <- x + 1
     if (input$cWorld) x <- x + 1
@@ -179,10 +205,20 @@ shinyServer(function(input, output) {
       n_col <- ceiling(sqrt(length(unique(d$region))))
       if (n_col > 3) n_col <- 3
 
-      legend_labels <- if (input$cIncludeDeath) {
+      legend_labels <- if (input$cIncludeDeath && !input$cSeparateDeath) {
         c(confirmed = "confirmed", recovered = "recovered and casualties")
+      } else if (input$cIncludeDeath && input$cSeparateDeath) {
+        c(confirmed = "confirmed", recovered = "recovered", death = "casualties")
       } else {
         c(confirmed = "confirmed", recovered = "recovered")
+      }
+
+      color_values <- if (input$cIncludeDeath && !input$cSeparateDeath) {
+        c("#e74c3c", "#27ae60")
+      } else if (input$cIncludeDeath && input$cSeparateDeath) {
+        c("#e74c3c", "#2980b9", "#27ae60")
+      } else {
+        c("#e74c3c", "#27ae60")
       }
 
       if (input$cOnePlot) {
@@ -201,7 +237,7 @@ shinyServer(function(input, output) {
         p <- ggplot(d, aes(x = date, y = cases, colour = type)) +
           geom_line() +
           # see::theme_abyss() +
-          scale_color_manual(values = c("#e74c3c", "#2980b9"), labels = legend_labels) +
+          scale_color_manual(values = color_values, labels = legend_labels) +
           scale_x_date(
             date_breaks = "1 week",
             date_labels = "%d.%m.",
